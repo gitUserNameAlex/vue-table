03473236c7eee5b6bb4f79d2b760ffa5a9b2c945
gitUserNameAlex <sashafootball2018@gmail.com>
2024-03-23 00:35:52 +0300

    final changes

--- src/components/UsersTableItem.vue
+++ src/components/UsersTableItem.vue
@@ -7,9 +7,15 @@
             <v-text-field v-model.number="editedItem.age" :readonly="!editing" variant="plain"/>
         </td>
         <td class="text-center">
-            <v-btn v-if="!editing" @click="editMode(true)" variant="outlined" color="primary" class="ma-2">Редактировать</v-btn>
-            <v-btn v-else @click="saveChanges" variant="outlined" color="success" class="ma-2">Сохранить</v-btn>
-            <v-btn @click="$emit('delete', item)" variant="outlined" color="error" class="ma-2">Удалить</v-btn>
+            <v-btn v-if="!editing" @click="editMode(true)" variant="outlined" color="primary" class="ma-2">
+                Редактировать
+            </v-btn>
+            <v-btn v-else @click="saveChanges" variant="outlined" color="success" class="ma-2">
+                Сохранить
+            </v-btn>
+            <v-btn @click="$emit('delete', item)" variant="outlined" color="error" class="ma-2">
+                Удалить
+            </v-btn>
         </td>
     </tr>
 </template>
